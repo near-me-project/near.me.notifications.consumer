4fbd167be5be9815bc71683b283c267fb9f186a2
tsekot sergey <tsekot.sergey@gmail.com>
2021-01-13 17:14:55 +0100

    initial

---
+++ app/controllers/AsyncController.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import javax.inject._
+
+import akka.actor.ActorSystem
+import play.api.mvc._
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future, Promise}
+
+@Singleton
+class AsyncController @Inject()(cc: ControllerComponents, actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends AbstractController(cc) {
+
+  def message = Action.async {
+    getFutureMessage(1.second).map { msg => Ok(msg) }
+  }
+
+  private def getFutureMessage(delayTime: FiniteDuration): Future[String] = {
+    val promise: Promise[String] = Promise[String]()
+    actorSystem.scheduler.scheduleOnce(delayTime) {
+      promise.success("Hi!")
+    }(actorSystem.dispatcher)
+    promise.future
+  }
+
+}
