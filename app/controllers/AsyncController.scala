cb83e3aadde704a58a9f0b3130e89270d88550a7
tsekot sergey <tsekot.sergey@gmail.com>
2021-01-14 21:27:10 +0100

    adding config hosts

--- app/controllers/AsyncController.scala
+++ app/controllers/AsyncController.scala
@@ -13,9 +13,9 @@ import scala.concurrent.{ExecutionContext, Future, Promise}
 @Singleton
 class AsyncController @Inject()(cc: ControllerComponents, actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends AbstractController(cc) {
 
-  def message(clientId: String, latitude: String, longitude: String) = Action.async {
+  def locationUpdatesFromClient(clientId: String, latitude: String, longitude: String) = Action.async {
     Future {
-      processIncomeLocationUpdates(LocationModel(clientId, latitude, longitude)).map(res => placeToQueue(res))
+      processIncomeLocationUpdates(LocationModel(clientId, latitude, longitude)).filter(res => res.nearestPlaces.nonEmpty).map(res => placeMessageToBrokerQueue(res))
       Ok
     }
   }
@@ -25,21 +25,21 @@ class AsyncController @Inject()(cc: ControllerComponents, actorSystem: ActorSyst
 
     actorSystem.scheduler.scheduleOnce(1.millisecond) {
 
-      goToDb(location).onComplete(promise.complete)
+      findAllNearestLocations(location).onComplete(promise.complete)
 
     }(actorSystem.dispatcher)
     promise.future
   }
 
-  private def goToDb(income: LocationModel): Future[LookupResult] = {
+  private def findAllNearestLocations(incomeReq: LocationModel): Future[LookupResult] = {
     Future {
-      val models: Seq[LocationModel] = new Repository().findByClientId(income.clientId).filter((location: LocationModel) => check(location, income))
-      println(s"Found locations: $models")
-      LookupResult(models.toList, income.clientId)
+      val locations: Seq[LocationModel] = new Repository().findByClientId(incomeReq.clientId).filter((location: LocationModel) => checkIfLocationCloseEnough(location, incomeReq))
+      println(s"Found locations: $locations")
+      LookupResult(locations.toList, incomeReq.clientId)
     }
   }
 
-  private def check(fromDb: LocationModel, fromMobile: LocationModel) = fromMobile.latitude == fromDb.latitude && fromMobile.longitude == fromDb.longitude
+  private def checkIfLocationCloseEnough(fromDb: LocationModel, fromMobile: LocationModel) = fromMobile.latitude == fromDb.latitude && fromMobile.longitude == fromDb.longitude
 
-  private def placeToQueue(result: LookupResult): Unit =  RabbitClient.getInstance().sendEventToQueue(result, RabbitClient.MOBILE_NOTIFICATIONS_QUEUE)
+  private def placeMessageToBrokerQueue(result: LookupResult): Unit =  RabbitClient.getInstance().sendEventToQueue(result, RabbitClient.MOBILE_NOTIFICATIONS_QUEUE)
 }
